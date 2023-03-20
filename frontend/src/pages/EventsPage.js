313152aea12781a47ca11dc80d811c3b78fec38e
Yash Pandya <yash.pandya@briskteq.com>
2023-03-20 16:59:20 +0530

    nested route added

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY = [
+  {
+    id: "e1",
+    title: "Exuburance",
+  },
+  {
+    id: "e2",
+    title: "Verve",
+  },
+];
 
 const EventsPage = () => {
-  return <h1>EventsPage</h1>;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
